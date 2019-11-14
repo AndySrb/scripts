1b30bd44363081ad51863f096550cb812a810061
AndySrb <andrejagrujic2001@gmail.com>
2019-11-14 17:19:52 +0100

    Convert between hexadecimal, binary and decimal values

--- py-hexbindec.py
+++ py-hexbindec.py
@@ -1,9 +1,9 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Convert between hex,bin and dec')
+parser = argparse.ArgumentParser(description='Convert between hexadecimal,binary and decimal values')
 parser.add_argument('-c','--convert', dest='convert', action='store',
 choices={'db','dh','bd','bh','hd','hb'},
-help='d= decimal b=binary h=hexdecimal first letter is form which format we are converting from seccond letter is desired format example: py-hexbindec.py -c dh -i 25 this will convert form decimal to hexdecimal')
+help='d= decimal b=binary h=hexadecimal first letter is form which format we are converting from seccond letter is desired format example: py-hexbindec.py -c dh -i 25 this will convert form decimal to hexadecimal')
 
 parser.add_argument('-i',dest='input',action='store',default=argparse.SUPPRESS, type=str, required=True)
 
